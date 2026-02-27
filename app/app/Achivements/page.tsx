0fabdd6265afb02a1931e9bb15bb956048d1f41d
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2026-02-27 15:35:45 +0800

    Delete app directory

--- app/app/Achivements/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ACHIVEMENTS = () => {
-  return (
-    <div></div>
-  )
-}
-
-export default ACHIVEMENTS
--- app/app/Skill/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SKILL = () => {
-  return (
-    <div></div>
-  )
-}
-
-export default SKILL
