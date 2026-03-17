cecab7d1a0e5224ea435838b78d0aa06fcd3c462
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2026-03-17 22:01:43 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
-import Navbar from "../components/Navbar";
+import "@/app/globals.css";
+
+import Navbar from "@/components/Navbar";
+
 import { SocialIcon } from "react-social-icons";
 import Link from "next/link";
 const geistSans = Geist({
